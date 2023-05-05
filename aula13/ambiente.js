6bc32b082edf69b240f3778a62bc8164e90a6461
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-05-05 09:54:11 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+// console.log("Tudo bem?")
+
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
