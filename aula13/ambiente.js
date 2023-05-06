2a92a87633c99b988fc6372fb7eaa1059ce62983
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-05-06 20:54:09 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -6,7 +6,14 @@
 // console.log("Tudo bem?")
 
 var c = 1
-while (c <= 5) {
-    console.log(`Passo ${c}`)
-    c++ 
-}
+// while (c <= 5) {
+//     console.log(`Passo ${c}`)
+//     c++ 
+// }
+
+do {
+    console.log(`passo ${c}`)
+    c++
+} while(c <= 5)
+
+
