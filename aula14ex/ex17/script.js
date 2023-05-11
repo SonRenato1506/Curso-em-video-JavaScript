c97ba0ff41ea306b4161b52a4e634d95ed0da731
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-05-11 09:55:38 -0300

    Update script.js

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,10 +1,10 @@
 var n
-var c
 var res = document.getElementById('res')
 function contar() {
     res.innerHTML = ""
     n = document.getElementById('num')
     n = Number(n.value)
+    c = 1
     while (c <= 10) {
         res.innerHTML += `<p>${n} x ${c} = ${n * c}</p>`
         c++
